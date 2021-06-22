e538f8cb0c0e42e4fa5f0eab17bdee2f9551654c
Dylan Rafael <dylanrafael05@gmail.com>
2021-06-22 13:29:40 -0400

    worked a *lot* on the main file

--- ReachPy/main.py
+++ ReachPy/main.py
@@ -1,5 +1,57 @@
-def main():
+import sys
+import inputs
+from busio import I2C
+from adafruit_pca9685 import *
+from adafruit_servokit import *
+
+i2c = None
+servos = None
+
+
+class StopRun(BaseException):
     pass
+
+def init():
+    #globals
+    global i2c, servos
+
+    #initialize
+    i2c = I2C(board.SCL, board.SDA)
+    servos = ServoKit(channels=16, i2c=i2c)
+
+
+def deinit():
+    pass
+
+def run():
+    pass
+
+
+
+
+def main():
+    # initialize
+    init()
+
+    # execution loop
+    while True:
+
+        try:
+            # attempt to run
+            run()
+
+        except object as e:
+            # catch real errors and print to stderr
+            if not isinstance(e, StopRun):
+                sys.stderr.write(str(e))
+
+            break
+
+        # ensure wait between execution cycles
+        time.sleep(10)
+
+    # deinit
+    deinit()
 
 if __name__ == '__main__':
     main()
